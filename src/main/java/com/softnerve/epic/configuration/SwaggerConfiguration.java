f63cdb5f60ccdc9f613f157983e602f7494d53cb
mianiket0612 <ANIKET.GOSAVI@softnerve.com>
2026-03-24 17:18:11 +0530

    Updated swagger configuration

--- src/main/java/com/softnerve/epic/configuration/SwaggerConfiguration.java
+++ src/main/java/com/softnerve/epic/configuration/SwaggerConfiguration.java
@@ -4,6 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Patient Microservice API",
@@ -12,19 +16,24 @@ import org.springframework.context.annotation.Configuration;
         servers = {
 
                 @Server(
-                        url = "http://localhost:8282/v1.0.0/apis/patient",
+                        url = "http://localhost:7979/v1.0.0/apis/fhir",
                         description = "Local server"
-                ),
-                @Server(
-                        url = "https://kong.softnerve.com/v1.0.0/apis/patient",
-                        description = "Prod Server"
-                ),
-                @Server(
-                        url = "https://softnerve.com/v1.0.0/apis/patient",
-                        description = "staging Server"
                 ),
         }
 )
 @Configuration
 public class SwaggerConfiguration {
+    /**
+     * Configures the Swagger Docket bean.
+     * This bean is used to generate API documentation for the controllers in the specified package.
+     *
+     * @return a Docket instance configured for Swagger 2 documentation.
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.softnerve.epic.controller"))
+                .build();
+    }
 }
